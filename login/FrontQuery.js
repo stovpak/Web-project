0defd31bb9b923e0d6d82929f0c3e302522a2d64
stovpak <47190281+stovpak@users.noreply.github.com>
2019-10-06 00:41:50 +0300

    classes and orm
    check pls

---
+++ login/FrontQuery.js
@@ -0,0 +1,7 @@
+class FrontQuery{
+    constructor(login,password,name,surname,id,rankId,date){
+        this.login=login;
+        this.rankId=rankId;
+    }
+};
+module.exports = FrontQuery;
--- login/UserData.js
+++ login/UserData.js
@@ -1,11 +1,11 @@
 class UserData{
-    constructor(roleId,login,password,name,surname,birthday){
+    constructor(roleId,login,password,name,surname,date){
         this.login=login;
         this.password=password;
         this.name=name;
         this.surname=surname;
         this.roleId=roleId;
-        this.birthday=birthday;
+        this.date=date;
     }
 };
 module.exports = UserData;
