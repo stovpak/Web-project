1d9c410a3f746ed61937301e313d95a2e3dd1b12
stovpak <47190281+stovpak@users.noreply.github.com>
2019-10-06 16:20:14 +0300

    fix orm

--- login/UserData.js
+++ login/UserData.js
@@ -1,11 +1,11 @@
 class UserData{
-    constructor(roleId,login,password,name,surname,date){
+    constructor(roleId,login,password,name,surname,birthday){
         this.login=login;
         this.password=password;
         this.name=name;
         this.surname=surname;
         this.roleId=roleId;
-        this.date=date;
+        this.birthday=birthday;
     }
 };
 module.exports = UserData;
