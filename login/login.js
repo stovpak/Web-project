1aac47b106105803869a5fc9503dd4a0f60806fb
stovpak <47190281+stovpak@users.noreply.github.com>
2019-10-06 16:20:14 +0300

    fix orm

--- login/login.js
+++ login/login.js
@@ -3,62 +3,110 @@ let mysql = require('mysql');
 let connectionConfig = require('./ConfigConnection.json');
 
 const connection = mysql.createConnection(connectionConfig);
-
-connection.connect(function(err){
-    if (err) {
-        return console.error("Ошибка: " + err.message);
-    }
-    else{
-        console.log("Подключение к серверу MySQL успешно установлено");
-    }
-});
+const LoginRequest  = require("./LoginRequest.js");
+const FrontQuery = require("./FrontQuery.js");
+const UserData = require("./UserData.js");
 
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("web", "root", "Edding16", {
+const sequelize = new Sequelize("web","root","Edding16", {
     dialect: "mysql",
     host: "localhost",
     logging: false,
     define: {
         timestamps: false,
-
     }
-
 });
 
 
 
 
-const User = sequelize.define("users", {
+    const Roles = sequelize.define("roles", {
+        id:{type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        role_name:{
+            type: Sequelize.STRING,
+            allowNull: false
+        }
+    });
+
+    const User = sequelize.define("users", {
     id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
     },
-    Name: {
+    first_name: {
         type: Sequelize.STRING,
+        allowNull: false
+    },
+    second_name:{
+        type: Sequelize.STRING,
+        allowNull: false,
 
     },
-    Date: {
+    role_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references:{
+            model: 'roles',
+            key: 'id'
+        }
+    },
+    login:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique:true
+    },
+    password:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique:true
+    },
+    birthday: {
         type: Sequelize.DATE,
-
-    },
-    RoleId:{type: Sequelize.INTEGER,},
-    Login:{type: Sequelize.STRING,},
-    Surname:{type: Sequelize.STRING,},
-    Password:{type: Sequelize.STRING,}
-
+        allowNull: false
+    }
 });
 
+    sequelize.sync().then(result=>{
+        console.log(result);
+    })
+        .catch(err=> console.log(err));
 
-const LoginRequest  = require("./LoginRequest.js");
-const FrontQuery = require("./FrontQuery.js");
-
-let loginRequest = new  LoginRequest('Vasya','Vasya123');
 
 
-function SingIn (Login,Password)
+
+/*function addUser(userData,User) {
+
+    User.create({
+        first_name: userData.name,
+        second_name:userData.surname,
+        birthday: userData.birthday,
+        login:userData.login,
+        password: userData.password,
+        role_id:userData.roleId
+    }).then(res=>{
+        console.log(res);
+    }).catch(err=>console.log(err));
+}
+
+let userData = new UserData(1,"Bob2123","Bob123","Jon","Travolta",1999-1-12);
+
+addUser(userData,User);
+*/
+
+
+let loginRequest = new  LoginRequest('Bob123','Bob123');
+
+
+function SingIn (loginRequest)
 {
-    User.findOne({raw:true,where: {Name: Login,Password:Password}})
+    User.findOne({raw:true,where: {login: loginRequest.login,password:loginRequest.password}})
         .then(User=>{
             if(!User){
                 console.log("Данные введены неправильно");
@@ -68,14 +116,14 @@ function SingIn (Login,Password)
                 let userTables = JSON.parse(JSON.stringify(User));
                 console.log(userTables);
                 let frontQuery = new FrontQuery();
-                frontQuery.rankId = userTables["RoleId"];
-                frontQuery.login = userTables["Login"];
+                frontQuery.rankId = userTables["role_id"];
+                frontQuery.login = userTables["login"];
                 console.log(frontQuery.rankId, frontQuery.login);
             }
             }).catch(err=>console.log(err));
 };
 
 
-SingIn(loginRequest.login,loginRequest.password);
+SingIn(loginRequest);
 
 
