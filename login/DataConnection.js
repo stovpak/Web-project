b4631bcbc6e2e7fd37eec0f31e98507a0128624d
stovpak <47190281+stovpak@users.noreply.github.com>
2019-10-09 18:54:15 +0300

    fix db password and models,connection location

--- login/DataConnection.js
+++ login/DataConnection.js
@@ -1,7 +1,5 @@
 const Sequelize = require("sequelize");
-require('dotenv').config();
-
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS, {
+const sequelize = new Sequelize("web","root",process.env.Db_Password, {
     dialect: "mysql",
     host: "localhost",
     logging: false,
--- login/login.js
+++ login/login.js
@@ -1,93 +1,15 @@
-let mysql = require('mysql');
-
-let connectionConfig = require('./ConfigConnection.json');
-
-const connection = mysql.createConnection(connectionConfig);
 const LoginRequest  = require("./LoginRequest.js");
 const UserResponse = require("./UserResponse.js");
 const UserData = require("./UserData.js");
-
-
-
-
-
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize("web","root","Edding16", {
-    dialect: "mysql",
-    host: "localhost",
-    logging: false,
-    define: {
-        timestamps: false,
-    }
-});
-
-
-
-    const Roles = sequelize.define("roles", {
-        id:{type: Sequelize.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false
-        },
-        role_name:{
-            type: Sequelize.STRING,
-            allowNull: false
-        }
-    });
-
-    const User = sequelize.define("users", {
-    id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
-    },
-    first_name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    second_name:{
-        type: Sequelize.STRING,
-        allowNull: false,
-
-    },
-    role_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references:{
-            model: 'roles',
-            key: 'id'
-        }
-    },
-    login:{
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique:true
-    },
-    password:{
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique:true
-    },
-    birthday: {
-        type: Sequelize.DATE,
-        allowNull: false
-    }
-});
-
-    sequelize.sync().then(result=>{
-        console.log(result);
-    })
-        .catch(err=> console.log(err));
-
-
+const DataModels = require("./DataModels");
 
 let loginRequest = new  LoginRequest('Bob123','Bob123');
 
 
+
 function SingIn (loginRequest)
 {
-    User.findOne({raw:true,where: {login: loginRequest.login,password:loginRequest.password}})
+    DataModels.User.findOne({raw:true,where: {login: loginRequest.login,password:loginRequest.password}})
         .then(User=>{
             if(!User){
                 console.log("Данные введены неправильно");
@@ -95,11 +17,9 @@ function SingIn (loginRequest)
             }
             else {
                 let userTables = JSON.parse(JSON.stringify(User));
-                console.log(userTables);
                 let frontQuery = new UserResponse();
                 frontQuery.rankId = userTables["role_id"];
                 frontQuery.login = userTables["login"];
-                console.log(frontQuery.rankId, frontQuery.login);
             }
             }).catch(err=>console.log(err));
 };
