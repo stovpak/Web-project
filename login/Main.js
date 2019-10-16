4a4f4e82f2d17cca3e254225d747bae6f19b7b84
stovpak <47190281+stovpak@users.noreply.github.com>
2019-10-16 20:22:49 +0300

    firs api try
    add new file Singin.now there is singin function  in main now first api

--- login/Main.js
+++ login/Main.js
@@ -1,32 +1,12 @@
+const singIn=require("./SingIn.js");
 const LoginRequest  = require("./LoginRequest.js");
-const UserResponse = require("./UserResponse.js");
-const UserData = require("./UserData.js");
-const UserModel = require("./Models/UserModel.js");
+const express = require("express");
+const app = express();
 
 
 let loginRequest = new  LoginRequest('Bob123','Bob123');
 
-function SingIn (loginRequest)
-{
-
-    UserModel.User.findOne({raw:true,where: {login: loginRequest.login,password:loginRequest.password}})
-        .then(User=>{
-            if(!User){
-                console.log("Данные введены неправильно");
-                return;
-            }
-            else {
-                let userTables = JSON.parse(JSON.stringify(User));
-                let userResponce = new UserResponse();
-                userResponce.roleId = userTables["role_id"];
-                userResponce.login = userTables["login"];
-                userResponce.id = userTables["id"];
-            }
-        }).catch(err=>console.log(err));
-};
-
-
-SingIn(loginRequest);
-
-
-
+app.listen(3000);
+app.get("/SingIn",function(request, response){
+    singIn.SingIn(loginRequest,request,response);
+});
