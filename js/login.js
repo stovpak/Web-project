11311f1b1bd2f5751633fa4387ffc87061cf8b2f
shevdary <shevdary@ukr.net>
2019-10-16 21:52:44 +0300

    use bootstrap in login page(pls. just see only index.html and css files)

---
+++ js/login.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+ class SingIn extends Component
+{
+    state = {
+
+    }
+    render ()
+    {
+        return (
+            <div className="container">
+                form.white 
+
+            </div>
+        )
+    }
+}
+
+export default SingIn
