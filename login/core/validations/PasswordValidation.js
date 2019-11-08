5be0a2ee066529d74eb092f340cc72019e049743
Alex Toryanik <alex1toryanik@gmail.com>
2019-11-08 20:47:47 +0200

    feature: add email and password validation

---
+++ login/core/validations/PasswordValidation.js
@@ -0,0 +1,25 @@
+const PasswordValidator = require('password-validator');
+
+const schema = new PasswordValidator();
+schema
+  .is().min(8)
+  .is().max(100)
+  .has()
+  .uppercase()
+  .has()
+  .lowercase()
+  .has()
+  .digits()
+  .has()
+  .not()
+  .spaces()
+  .has()
+  .not()
+  .symbols();
+function PasswordValidation(password) {
+  if (password.match(/([а-я,' '])/gi) === null && schema.validate(password)) {
+    return true;
+  }
+  return false;
+}
+module.exports.PasswordValidation = PasswordValidation;
