19b0b0fd3ca6e341c09f8a8c3352a7a57ed8bc12
Daryna <shevvdary@gmail.com>
2020-07-31 00:30:45 +0300

    add component restore password

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,63 +1,64 @@
 import React, { Component } from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect,
-    Link
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link
 } from "react-router-dom";
-import {createBrowserHistory} from 'history';
+import { createBrowserHistory } from "history";
 import SignIn from "./components/signIn/SignIn";
 import SignUp from "./components/signUp/SignUp";
 import MainPage from "./components/Main-Page/Home";
+import Cookies from "universal-cookie";
 import CreateTopic from "./components/topics/createTopic";
 import Profile from "./components/userProfile/Profile";
 import ChangeEmail from "./components/changeData/СhangeEmail";
 import ChangeUserPassword from "./components/changeData/ChangePassword";
-import {getJwt} from "./components/helpers/userService";
+import { getJwt } from "./components/helpers/userService";
+import Paginate from "./components/helpers/paginate";
 import ChangeData from "./components/changeData/ChangeData";
-import ChangeUserEmail from "./components/changeData/ChangeEmail";
+import RestorePassword from "./components/restorePassword/RestorePassword";
 
 let history = new createBrowserHistory({
-    baseName:'/'
+  baseName: "/"
 });
 
 export default class App extends Component {
-    render() {
-        const isAuth = getJwt();
-        return (
-            <Router history={history}>
-                <Switch>
-                    <Route
-                        exact
-                        path="/"
-                        component={() =>
-                            isAuth ? (
-                                <Redirect to={"/topics"} />
-                            ) : (
-                                <Redirect to={"user/sign-in"} />
-                            )
-                        }
-                    />
-                    <Route path={"/user/sign-up"} component={SignUp} />
-                    <Route path={"/user/sign-in"} component={SignIn} />
-                    <Route exact path={"/topics"}  component={MainPage} />
-                    <Route path={"/topics/create-topic"} component={CreateTopic} />
-                    <Route exact path={"/user/profile"} component={Profile} />
-                    <Route
-                        path={"/user/profile/change-email"}
-                        component={ChangeUserEmail}
-                    />
-                    <Route
-                        path={"/user/profile/change-pass"}
-                        component={ChangeUserPassword}
-                    />
-                    <Route
-                        path={"/user/profile/change-data"}
-                        component={ChangeData}
-                    />
-                </Switch>
-            </Router>
-        );
-    }
+  render() {
+    const isAuth = getJwt();
+    return (
+      <Router history={history}>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={() =>
+              isAuth ? (
+                <Redirect to={"/topics"} />
+              ) : (
+                <Redirect to={"user/sign-in"} />
+              )
+            }
+          />
+          <Route path={"/user/sign-up"} component={SignUp} />
+          <Route exact path={"/user/sign-in"} component={SignIn} />
+          <Route exact path={"/topics"} component={MainPage} />
+          <Route path={"/topics/create-topic"} component={CreateTopic} />
+          <Route exact path={"/user/profile"} component={Profile} />
+          <Route path={"/user/profile/change-email"} component={ChangeEmail} />
+          <Route
+            path={"/user/profile/change-pass"}
+            component={ChangeUserPassword}
+          />
+          <Route path={"/user/profile/change-data"} component={ChangeData} />
+          <Route path={"/user/profile/change-data"} component={ChangeData} />
+          <Route
+            path={"/user/sign-in/forget-password"}
+            component={RestorePassword}
+          />
+        </Switch>
+      </Router>
+    );
+  }
 }
--- front/src/components/restorePassword/RestorePassword.jsx
+++ front/src/components/restorePassword/RestorePassword.jsx
@@ -1,83 +1,43 @@
 import React, { Component } from "react";
-import { useFormik, Formik, Field, Form, ErrorMessage } from "formik";
+import { useFormik } from "formik";
 import "./restorePasswordStyle.css";
 import img from "./passwordPNG.png";
-import * as Yup from "yup";
-import AuthApi from "../helpers/authApi";
-import { emailSave, restorePasswordInfo } from "../helpers/userService";
-import { redirectToUrl } from "../helpers/baseAPI";
+const RestorePassword = () => {
+  const formik = useFormik({
+    initialValues: {
+      password: "",
+      confirmPassword: "",
+      passwordKey: "",
+      passwordMatch: "",
+      email: ""
+    },
+    validate: values => {},
+    onSubmit: values => {}
+  });
+  return (
+    <div id={"main"}>
+      <div className=" p-3 mb-5 bg-light rounded container w-40">
+       <div><img
+          src={img}
+          alt="passwordRestore"
+          className={"img-size text-center"}
+        />
+       </div>
+          <div>
 
-export default class RestorePassword extends Component {
-    render() {
-        return (
-            <div className="main-bg">
-                <div className=" p-3 mb-5 bg-light rounded container w-35 display-center ">
-                    <div>
-                        <img
-                            src={img}
-                            alt="passwordRestore"
-                            className={"img-size text-center"}
-                        />
-                    </div>
-                    <div>
-                        <h1 className="text-center">Забыли пароль?</h1>
-                        <h5 className="text-black-50 text-center mt-lg-5">
-                            Введите почту, чтобы востановить пароль
-                        </h5>
-                        <Formik
-                            initialValues={{ email: "", errorMessage:"" }}
-                            validationSchema={Yup.object({
-                                email: Yup.string()
-                                    .email("Почта введена неправильно")
-                                    .required("Поле не должно быть пустым")
-                            })}
-                            onSubmit={({ email , errorMessage}, { setStatus, setSubmitting }) => {
-                                setStatus();
-                                AuthApi.restorePassword(email)
-                                    .then(res => {
-                                        if (res) {
-                                            restorePasswordInfo.email = email;
-                                            redirectToUrl("sign-in/restore-password/send-key");
-                                        }
-                                    })
-                                    .catch(err => {
-                                        if (err.response.status>=400){
-                                            alert("Такой почты не cуществует");
-                                        }
-                                        setSubmitting(false);
-                                        setStatus(err);
-                                    });
-                            }}
-                            render={({ errors, status, touched, isSubmitting }) => (
-                                <Form className="form-group text-center center-component mt-lg-5 phone-size">
-                                    <div className="mb-5 ">
-                                        <Field
-                                            type="email"
-                                            name="email"
-                                            placeholder="Почта"
-                                            className={"form-control "}
-                                        />
-                                        <ErrorMessage
-                                            name={"email"}
-                                            component={"div"}
-                                            className="text-danger font-italic position-fixed small-text"
-                                        />
+                  <h1 className="text-center">Забыли пароль?</h1>
+                  <h5 className="text-black-50 text-center">Введите почту, чтобы востановить пароль</h5>
+              <form onSubmit={formik.handleSubmit} className="form-group text-center">
+                      <input type="email"
+                      placeholder="Почта"
+                      className="form-control center-component"/>
 
-                                    </div>
-
-                                    <button
-                                        type="submit"
-                                        className="btn btn-warning  w-75 phone-size"
-                                    >
-                                        Выслать код
-                                    </button>
-                                </Form>
-                            )}
-                        />
-                    </div>
-                </div>
-            </div>
-        );
-    }
+                      <button className="btn btn-warning mt-3">Выслать код</button>
+              </form>
+          </div>
+      </div>
+    </div>
+  );
 };
 
+export default RestorePassword;
