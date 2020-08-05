8a2fe7428139ace9271e4a41c0cdfd9119cd3fb1
Daryna <shevvdary@gmail.com>
2020-08-05 14:24:07 +0300

    fix: Path attribute for cookies

--- front/src/components/helpers/userService.js
+++ front/src/components/helpers/userService.js
@@ -6,13 +6,13 @@ export function getCookiesName(name) {
   return cookies.get("username");
 }
 export function setCookiesName(name) {
-  return cookies.set("username", name);
+  return cookies.set("username", name, {path:"/"});
 }
 export const getJwt = () => {
   return cookies.get("sessionToken");
 };
 export const setSession = token => {
-  return cookies.set("sessionToken", token);
+  return cookies.set("sessionToken", token,{path:"/"});
 };
 
 export let AuthRequest = { login: "", password: "" };
