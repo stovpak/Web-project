9324301f8edfe8830c20d12e97f202231f9d7fdc
Shevdary <Darena080720>
2021-03-02 19:04:08 +0200

    add folder for navigation on app

---
+++ front/src/navigation/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import Comments from 'components/Comments';
+import NavBar from 'components/NavBar';
+import TopicsRouter from './TopicRouter';
+import UserRouter from './UserAuth';
+
+let history = new createBrowserHistory({
+  baseName: '/',
+});
+
+const App = () => {
+  const { pathname } = history.location;
+
+  return (
+    <>
+      {!pathname.includes('sign-in') && !pathname.includes('sign-up') && (
+        <NavBar />
+      )}
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/" component={() => <Redirect to="/topics" />} />
+          <Route path="/topics" component={TopicsRouter} />
+          <Route path="/user" component={UserRouter} />
+          <Route path="/topicId/show-comment" component={Comments} />
+          {/*  <Route path="/topics/my-topics" component={UserTopics} />*/}
+        </Switch>
+      </Router>
+    </>
+  );
+};
+export default App;
