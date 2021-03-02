6c6b23aab220bdb0cac416aa60099b82d19ff857
Shevdary <Darena080720>
2021-03-02 19:01:56 +0200

    update comments on page

--- front/src/components/Comments/components/Messages/index.jsx
+++ front/src/components/Comments/components/Messages/index.jsx
@@ -1,59 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { userSelector } from '../../../../redux/user/selector';
-const Messages = ({ content, editComments, deleteComments }) => {
-  const user = useSelector(userSelector);
+import { userSelector } from 'redux/user/selector';
+import avatarLabel from 'assets/images/avatar.svg';
 
-  let showDate, showTime;
-  const messageItem = content.map(({ author_name, date, text, id }) => {
-    if (date !== null) {
-      let data = new Date(date);
-      showDate =
-        data.getDate() +
-        ' ' +
-        data.toLocaleString('ru', { month: 'long' }) +
-        ' ' +
-        data.getFullYear();
-      showTime = data.getHours() + ':' + data.getMinutes();
-    }
-    return (
-      <div className="media text-muted pt-3  " key={id}>
-        <svg
-          className="bd-placeholder-img mr-2 rounded"
-          width="32"
-          height="32"
-          xmlns="http://www.w3.org/2000/svg"
-          preserveAspectRatio="xMidYMid slice"
-          focusable="false"
-          role="img"
-          aria-label="Placeholder: 32x32"
-        >
-          <title>Placeholder</title>
-          <rect width="100%" height="100%" fill="#ec971f"></rect>
-        </svg>
-        <div className="pb-3 mb-0  border-bottom border-gray pb-2 mb-0 w-100 cc_cursor">
-          <strong className="d-block text-body cc_cursor blog-post-title text-monospace">
-            @{author_name}
-          </strong>
-          {text}
-          <small className="text-muted float-right">
-            {author_name === user.username && (
-              <>
-                <button type="button" onClick={() => editComments(id)}>
-                  Изменить
+const Messages = ({ comments, editComments, deleteComments }) => {
+  const user = useSelector(userSelector);
+  const [isShow, setIsShow] = useState();
+  const [newComments, setNewComments] = useState(null);
+
+  const handleSaveMessage = (id, newMessage) => {
+    setIsShow(false);
+    editComments(id, newMessage);
+  };
+
+  return (
+    <div className="media text-muted pt-3">
+      <img
+        src={avatarLabel}
+        alt=""
+        className="bd-placeholder-img mr-2 rounded"
+      />
+      <div className="pb-3 mb-0  border-bottom border-gray pb-2 mb-0 w-100 cc_cursor">
+        <strong className="d-block text-body cc_cursor blog-post-title text-monospace">
+          @{comments.author_name}
+        </strong>
+        {isShow ? (
+          <input
+            type="text"
+            name="message"
+            onChange={e => setNewComments(e.target.value)}
+          />
+        ) : (
+          comments.text
+        )}
+        <small className="text-muted float-right">
+          {comments.author_name === user.username && (
+            <>
+              {isShow ? (
+                <button
+                  type="button"
+                  onClick={() => handleSaveMessage(comments.id, newComments)}
+                >
+                  Сохранить
                 </button>
-                <button type="button" onClick={() => deleteComments(id)}>
-                  Удалить
+              ) : (
+                <button type="button" onClick={setIsShow}>
+                  Редактировать
                 </button>
-              </>
-            )}
-            {showTime} {showDate}
-          </small>
-        </div>
+              )}
+              <button type="button" onClick={() => deleteComments(comments.id)}>
+                Удалить
+              </button>
+            </>
+          )}
+        </small>
       </div>
-    );
-  });
-  return <div>{messageItem}</div>;
+    </div>
+  );
 };
 
 export default Messages;
